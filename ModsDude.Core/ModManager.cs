77de21a79072a629e4bc65961caf831c3947e121
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2022-01-21 16:09:32 +0100

    Add project files.

---
+++ ModsDude.Core/ModManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModsDude.Core;
+
+public class ModManager
+{
+    readonly IRemote _remote;
+    readonly IEnumerable<IModDescriptor> _cache;
+    readonly IEnumerable<IModDescriptor> _active;
+
+
+    public ModManager(IRemote remote, IEnumerable<IModDescriptor> cache, IEnumerable<IModDescriptor> active)
+    {
+        _remote = remote;
+        _cache = cache;
+        _active = active;
+    }
+}
